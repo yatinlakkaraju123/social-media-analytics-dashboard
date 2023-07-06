89eb57db82ad962e658caa5001a845df53febc2e
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2023-07-06 21:40:29 +0530

    first graph

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from dash import Dash, html
+# Import packages
+from dash import Dash, html, dash_table,dcc
+import pandas as pd
+import plotly.express as px
+# Incorporate data
+df = pd.read_csv('https://raw.githubusercontent.com/yatinlakkaraju123/post-scrapping/main/linkedinscrapper/data/postspider/postspider_2023-07-05T10-57-40.csv')
 
+# Initialize the app
 app = Dash(__name__)
 
+# App layout
 app.layout = html.Div([
-    html.Div(children='Hello World')
+    html.Div(children='LINKEDIN DASHBOARD'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
+    dcc.Graph(figure=px.histogram(df, x='hashtag1', y='likes', histfunc='avg'))
 ])
 
+# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
