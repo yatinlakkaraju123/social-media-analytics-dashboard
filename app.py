c6e3912f213ff11575bacbf051fca3b2971ca9c2
yatinlakkaraju123 <96581782+yatinlakkaraju123@users.noreply.github.com>
2023-07-10 22:02:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import plotly.express as px
 # Incorporate data
 df = pd.read_csv('https://raw.githubusercontent.com/yatinlakkaraju123/post-scrapping/main/linkedinscrapper/data/postspider/modified_file.csv')
 df.drop(df.columns[[0,1,3]],axis=1,inplace=True)
+#df_sorted = df.sort_values('likes',ascending=False)
 # Initialize the app
 app = Dash(__name__)
 
@@ -12,7 +13,7 @@ app = Dash(__name__)
 app.layout = html.Div([
     html.Div(children='LINKEDIN DASHBOARD'),
     dash_table.DataTable(data=df.to_dict('records'), page_size=10),
-    dcc.Graph(figure=px.histogram(df, x=['hashtag1','hashtag2','hashtag3','hashtag4','hashtag5','hashtag6','hashtag7','hashtag8','hashtag9','hashtag10'], y='likes', histfunc='avg'))
+    dcc.Graph(figure=px.histogram(df, x=['hashtag1','hashtag2','hashtag3','hashtag4','hashtag5','hashtag6','hashtag7','hashtag8','hashtag9','hashtag10'], y='likes',range_y=[600,8000]).update_xaxes(categoryorder='total descending'))
 ])
 
 # Run the app
