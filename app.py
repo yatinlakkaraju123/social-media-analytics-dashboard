c4ecd315bbe532e5ee9cebd9e1b7eceefd33a4cf
yatinlakkaraju123 <96581782+yatinlakkaraju123@users.noreply.github.com>
2023-07-11 10:38:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
 # Import packages
-from dash import Dash, html, dash_table,dcc
+from dash import Dash, html, dcc
 import pandas as pd
 import plotly.express as px
+
 # Incorporate data
 df = pd.read_csv('https://raw.githubusercontent.com/yatinlakkaraju123/post-scrapping/main/linkedinscrapper/data/postspider/modified_file.csv')
-df.drop(df.columns[[0,1,3]],axis=1,inplace=True)
-#df_sorted = df.sort_values('likes',ascending=False)
+df.drop(df.columns[[0, 1, 3]], axis=1, inplace=True)
+
+# Reshape the data to get all hashtags
+hashtag_columns = ['hashtag1', 'hashtag2', 'hashtag3', 'hashtag4', 'hashtag5', 'hashtag6', 'hashtag7', 'hashtag8', 'hashtag9', 'hashtag10']
+hashtags = df[hashtag_columns].values.flatten()
+hashtags_series = pd.Series(hashtags).dropna()
+
+# Calculate the sum of likes for each hashtag
+hashtags_likes = hashtags_series.value_counts().reset_index()
+hashtags_likes.columns = ['hashtag', 'likes']
+hashtags_likes.sort_values(by='likes', ascending=False, inplace=True)
+top_5_hashtags = hashtags_likes.head(10)
+
 # Initialize the app
 app = Dash(__name__)
 
 # App layout
 app.layout = html.Div([
     html.Div(children='LINKEDIN DASHBOARD'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
-    dcc.Graph(figure=px.histogram(df, x=['hashtag1','hashtag2','hashtag3','hashtag4','hashtag5','hashtag6','hashtag7','hashtag8','hashtag9','hashtag10'], y='likes',range_y=[600,8000]).update_xaxes(categoryorder='total descending'))
+    dcc.Graph(
+        figure=px.bar(top_5_hashtags, x='hashtag', y='likes'),
+    ),
 ])
 
 # Run the app
