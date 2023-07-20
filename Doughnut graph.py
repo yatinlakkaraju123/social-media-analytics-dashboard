9ee5ba336f92518871bd3ba080c274c7cf119468
Shaurya Pandey <127345568+Shaurya2510@users.noreply.github.com>
2023-07-20 21:28:47 +0530

    Add files via upload

---
+++ Doughnut graph.py
@@ -0,0 +1,32 @@
+import dash
+from dash import dcc
+from dash import html
+import pandas as pd
+import plotly.graph_objs as go
+
+df = pd.read_csv('modified_file.csv')
+
+combined_hashtags = pd.concat([df[col] for col in df.columns if col.startswith('hashtag')])
+
+hashtags_counts = combined_hashtags.value_counts()
+
+top_10_hashtags = hashtags_counts.head(10)
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    
+    dcc.Graph(
+        id='top-10-hashtags-pie',
+        figure={
+            'data': [
+                go.Pie(labels=top_10_hashtags.index, values=top_10_hashtags.values, hole=0.5)
+            ],
+            'layout': go.Layout(title='Top 10 Hashtags')
+        }
+    )
+])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ pie chart.py
@@ -0,0 +1,38 @@
+import dash
+from dash import dcc
+from dash import html
+import pandas as pd
+import plotly.graph_objs as go
+
+
+df = pd.read_csv('modified_file.csv')
+
+
+combined_hashtags = pd.concat([df[col] for col in df.columns if col.startswith('hashtag')])
+
+
+hashtags_counts = combined_hashtags.value_counts()
+
+
+top_10_hashtags = hashtags_counts.head(10)
+
+
+app = dash.Dash(__name__)
+
+
+app.layout = html.Div([
+    
+    dcc.Graph(
+        id='top-10-hashtags-pie',
+        figure={
+            'data': [
+                go.Pie(labels=top_10_hashtags.index, values=top_10_hashtags.values)
+            ],
+            'layout': go.Layout(title='Top 10 Hashtags (Pie Chart)')
+        }
+    )
+])
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
